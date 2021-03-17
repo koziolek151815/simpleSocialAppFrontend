d51e8ba552d790e5798c38406411212ebe7d3666
koziolek151815 <koziolek151815x@gmail.com>
2021-03-17 23:59:04 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState} from "react";
+import axios, * as others from 'axios';
+
+
 
 function App() {
+    const [data, setData] = useState([]);
+
+    const fetchData = async () => {
+        const response = await axios.get('http://localhost:8081/posts');
+        setData(response.data);
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>
+            <button className="fetch-button" onClick={fetchData}>
+                Fetch Data
+            </button>
+        </div>
+        {data.map(data =>  <div style={{ display:'flex', alignItems: 'center', justifyContent:'center', flexDirection: 'column'}}  key = {data.id}>
+                <p>{data.id}</p>
+                <p>{data.text}</p>
+            </div>
+        )}
     </div>
   );
 }
